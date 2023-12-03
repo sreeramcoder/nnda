4a9a23e30e8680397b0a0bb1e0e3cbe6a9b99ada
sreeramvenkat <sreeramvenkat@yahoo.com>
2023-12-03 14:27:08 +0530

    change to elixir :)

---
+++ lib/nnda2.ex
@@ -0,0 +1,18 @@
+defmodule Nnda2 do
+  @moduledoc """
+  Documentation for `Nnda2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nnda2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nnda2_test.exs
@@ -0,0 +1,8 @@
+defmodule Nnda2Test do
+  use ExUnit.Case
+  doctest Nnda2
+
+  test "greets the world" do
+    assert Nnda2.hello() == :world
+  end
+end
